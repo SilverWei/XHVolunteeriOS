088517dbe4823c0d78b11a15d8fdaa6a18d4ce03
silveracgal <silveracgal@gmail.com>
2015-06-05 12:49:44 +0800

    摄像头功能

--- XHVolunteeriOS/QRcodeViewController.swift
+++ XHVolunteeriOS/QRcodeViewController.swift
@@ -7,13 +7,45 @@
 //
 
 import UIKit
+import AVFoundation
 
-class QRcodeViewController: UIViewController {
+class QRcodeViewController: UIViewController,AVCaptureMetadataOutputObjectsDelegate {
 
+    var captureSession:AVCaptureSession?
+    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView:UIView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        var error:NSError?
+        let input:AnyObject! = AVCaptureDeviceInput.deviceInputWithDevice(captureDevice, error: &error)
+        
+        if(error != nil)
+        {
+            println("\(error?.localizedDescription)")
+            return
+        }
+        
+        captureSession = AVCaptureSession()
+        captureSession?.addInput(input as AVCaptureInput)
+        
+        let captureMetadataOutput = AVCaptureMetadataOutput()
+        captureSession?.addOutput(captureMetadataOutput)
+        
+        captureMetadataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+        captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        view.layer.addSublayer(videoPreviewLayer)
+        
+        captureSession?.startRunning()
+        
     }
 
     override func didReceiveMemoryWarning() {
